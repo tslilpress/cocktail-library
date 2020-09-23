78fba01514e48c2f5f71e0ace6736ab4e0dad1ff
tslilpress <tslilpress@gmail.com>
2020-09-23 16:23:04 -0400

    Edited the heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://mysterious-wildwood-12374.herokuapp.com/',
+  production: 'https://mysterious-wildwood-12374.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
